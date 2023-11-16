dc4fae82cf5e6fb0f72810b9852a73c19ba6c521
Soulshaker1 <143871322+Soulshaker1@users.noreply.github.com>
2023-11-16 20:58:43 +0300

    Add files via upload

---
+++ project/news/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+# Импортируем созданное нами представление
+from .views import PostView, PostCategoryView, CommentView, ArticleView, SearchView, create_post, PostUpdate, PostDelete
+
+
+urlpatterns = [
+   # path — означает путь.
+   # В данном случае путь ко всем товарам у нас останется пустым,
+   # чуть позже станет ясно почему.
+   # Т.к. наше объявленное представление является классом,
+   # а Django ожидает функцию, нам надо представить этот класс в виде view.
+   # Для этого вызываем метод as_view.
+   path('', PostView.as_view(), name='post_list'),
+   # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+   # int — указывает на то, что принимаются только целочисленные значения
+   path('<int:pk>', PostCategoryView.as_view()),
+   path('comment<int:pk>', CommentView.as_view()),
+   path('article/<int:pk>', ArticleView.as_view()),
+   path('search', SearchView.as_view()),
+   path('create/', create_post),
+   path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+]
---
+++ project/news/views.py
@@ -0,0 +1,132 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, PostCategory, Comment
+from datetime import datetime
+from .filters import PostFilter
+from django.shortcuts import render
+from .forms import PostForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+
+class PostView(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-dateCreation'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'post.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+    paginate_by = 2  # вот так мы можем указать количество записей на странице
+
+    def get_queryset(self):
+       # Получаем обычный запрос
+       queryset = super().get_queryset()
+       # Используем наш класс фильтрации.
+       # self.request.GET содержит объект QueryDict, который мы рассматривали
+       # в этом юните ранее.
+       # Сохраняем нашу фильтрацию в объекте класса,
+       # чтобы потом добавить в контекст и использовать в шаблоне.
+       self.filterset = PostFilter(self.request.GET, queryset)
+       # Возвращаем из функции отфильтрованный список товаров
+       return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['filterset'] = self.filterset
+        return context
+   # Переопределяем функцию получения списка товаров
+class PostCategoryView(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = PostCategory
+    # Используем другой шаблон — post.html
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post_detail'
+
+class CommentView(DetailView):
+   # Модель всё та же, но мы хотим получать информацию по отдельному товару
+   model = Comment
+   # Используем другой шаблон — post.html
+   template_name = 'comment.html'
+   # Название объекта, в котором будет выбранный пользователем продукт
+   context_object_name = 'comment'
+
+class ArticleView(DetailView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-dateCreation'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'article.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'Article'
+
+class SearchView(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+        # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-dateCreation'
+        # Указываем имя шаблона, в котором будут все инструкции о том,
+        # как именно пользователю должны быть показаны наши объекты
+    template_name = 'search.html'
+        # Это имя списка, в котором будут лежать все объекты.
+        # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'search'
+
+    def get_queryset(self):
+       # Получаем обычный запрос
+       queryset = super().get_queryset()
+       # Используем наш класс фильтрации.
+       # self.request.GET содержит объект QueryDict, который мы рассматривали
+       # в этом юните ранее.
+       # Сохраняем нашу фильтрацию в объекте класса,
+       # чтобы потом добавить в контекст и использовать в шаблоне.
+       self.filterset = PostFilter(self.request.GET, queryset)
+       # Возвращаем из функции отфильтрованный список товаров
+       return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['filterset'] = self.filterset
+        return context
+
+def create_post(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/news/')
+
+    return render(request, 'product_edit.html', {'form': form})
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'product_edit.html'
+
+# Представление удаляющее товар.
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
